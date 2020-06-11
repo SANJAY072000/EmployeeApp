2cf703ec23cb32d77d33bbc019676a3d3e3f79e9
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-06-11 21:07:40 +0530

    Done with App

--- src/Emp.js
+++ src/Emp.js
@@ -9,34 +9,33 @@ class Emp extends Component{
     render(){
     return (
     <FlatList data={Object.values(this.props.data)}
-    renderItem={({item})=>{
-      return <Card style={styles.container}>
-      <View style={styles.idcontainer}>
-      <Text style={styles.idtext}>
+    renderItem={({item})=>
+      <Card style={styles.container}>
+      <View style={styles.idContainer}>
+      <Text style={styles.idText}>
       {item.empid}
       </Text>
       </View>
-      <View style={styles.namecontainer}>
-      <Text style={styles.nametext}>
+      <View style={styles.nameAndSalaryContainer}>
+      <Text style={styles.nameText}>
       {`Name : ${item.empName}`}
       </Text>
-      <Text style={styles.saltext}>
+      <Text style={styles.salaryText}>
       {`Salary : ${item.salary.toFixed(2)}`}
       </Text>
       </View>
-      <View style={styles.percontainer}>
+      <View style={styles.performanceIconContainer}>
       <TouchableOpacity onPress={()=>this.props.badp(item.empid)}>
       <Icon android='md-thumbs-down' ios='ios-thumbs-down'
-       style={styles.badp}/>
+       style={styles.badPerformance}/>
       </TouchableOpacity>
       <TouchableOpacity onPress={()=>this.props.goodp(item.empid)}>
       <Icon android='md-thumbs-up' ios='ios-thumbs-up'
-       style={styles.goodp}/>
+       style={styles.goodPerformance}/>
       </TouchableOpacity>
       </View>
-      </Card>
-    }}
-    keyExtractor={item=item.empid.toString()}
+      </Card>}
+    keyExtractor={item=>item.empid.toString()}
     />
           );
     }
@@ -62,10 +61,54 @@ function mapDispatchToProps(dispatch){
 export default connect(mapStateToProps,mapDispatchToProps)(Emp);
 
 const styles = StyleSheet.create({
+  //list card view container
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "row"
   },
+  // no. of employee container
+  idContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 20
+  },
+  // no. of employee text
+  idText: {
+    fontSize: 18,
+    color: "#000"
+  },
+  // name and salary text container
+  nameAndSalaryContainer: {
+    flex: 4,
+    padding: 20
+  },
+  // employee name text
+  nameText: {
+    fontSize: 16,
+    color: "#000"
+  },
+  // employee salary text
+  salaryText: {
+    fontSize: 16,
+    color: "#000"
+  },
+  // like dislike icon container
+  performanceIconContainer: {
+    flex: 2,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  //like icon
+  goodPerformance: {
+    color: "green"
+  },
+  // dislike icon
+  badPerformance: {
+    color: "red"
+  }
 });
+

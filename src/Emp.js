3745eabf65fd480f35e8b3c4c23482159f75ebc8
Sanjay Singh Bisht <sanjaysinghbisht114@gmail.com>
2020-06-11 20:54:33 +0530

    Done with JSX of app

--- src/Emp.js
+++ src/Emp.js
@@ -2,14 +2,42 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View, 
   FlatList, TouchableOpacity } from 'react-native';
 import {connect} from 'react-redux';
+import {Card,Icon} from 'native-base';
 
 
 class Emp extends Component{
     render(){
     return (
-    <View style={styles.container}>
-        <Text>Welcome to Emp</Text>
-    </View>
+    <FlatList data={Object.values(this.props.data)}
+    renderItem={({item})=>{
+      return <Card style={styles.container}>
+      <View style={styles.idcontainer}>
+      <Text style={styles.idtext}>
+      {item.empid}
+      </Text>
+      </View>
+      <View style={styles.namecontainer}>
+      <Text style={styles.nametext}>
+      {`Name : ${item.empName}`}
+      </Text>
+      <Text style={styles.saltext}>
+      {`Salary : ${item.salary.toFixed(2)}`}
+      </Text>
+      </View>
+      <View style={styles.percontainer}>
+      <TouchableOpacity onPress={()=>this.props.badp(item.empid)}>
+      <Icon android='md-thumbs-down' ios='ios-thumbs-down'
+       style={styles.badp}/>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={()=>this.props.goodp(item.empid)}>
+      <Icon android='md-thumbs-up' ios='ios-thumbs-up'
+       style={styles.goodp}/>
+      </TouchableOpacity>
+      </View>
+      </Card>
+    }}
+    keyExtractor={item=item.empid.toString()}
+    />
           );
     }
 }
